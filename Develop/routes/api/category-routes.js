fc19092a9206822c591cab5672e25614c9f3f3da
Carter <carreejoh4637@gmail.com>
2023-05-23 16:13:04 -0600

    Finally understand router.get id, used an async function like the one in the classwork

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -11,9 +11,20 @@ router.get('/', (req, res) => {
   });
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id);
+    if (!categoryData) {
+      res.status(404).json({ message: "This category doesn't exist"});
+      return;
+    } else {
+      res.status(200).json(categoryData);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
