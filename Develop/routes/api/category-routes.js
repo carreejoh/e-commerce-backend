8d616e5d259a538b7bb823dba0f7fa269cc82bb9
Carter <carreejoh4637@gmail.com>
2023-05-23 16:33:21 -0600

    category routes finished, insomnia working perfectly, category model almost done, just need to create relationship with products

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -27,16 +27,49 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create({
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(newCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+  .then((updatedCategory) => {
+    res.json(updatedCategory);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((deletedCategory) => {
+    res.json(`${deletedCategory} I've been destroyed`);
+  })
+  .catch((err) => res.json(err));
 });
 
 module.exports = router;
